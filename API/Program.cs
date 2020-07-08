a9914636ba73c57f7b2f161802dbdb2f3bf965fd
dbyilla69 <doranyilla@gmail.com>
2020-07-08 16:05:01 -0500

    finish adding basic CRUD to app

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -28,7 +24,7 @@ namespace API
                 }
                 catch (Exception ex)
                 {
-                    
+
                     var logger = services.GetRequiredService<ILogger<Program>>();
                     logger.LogError(ex, "An error occured during migration");
                 }
